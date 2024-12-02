dacf0d5f88017994ee7e46125cf4c3815bc2ace7
Sivayogeith Umamaheswaran <65064458+Sivayogeith@users.noreply.github.com>
2024-12-02 21:05:04 +0530

    Added people, chance and text.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import 'bootstrap/dist/css/bootstrap.css';
+	import scriptSrc from 'bootstrap/dist/js/bootstrap.bundle.js?url';
+</script>
+
+<style>
+	:root {
+		--bs-primary: #198754;
+		--bs-primary-rgb: 25, 135, 84;
+	}
+</style>
+
+<svelte:head>
+	<script src={scriptSrc}></script>
+</svelte:head>
+
+<slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,74 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let dropdowns = [1];
+	let perkDropdowns = [[1]];  // Array of arrays, each representing perk dropdowns for a person
+	let perks: object = {};
+
+	const addDropdown = () => {
+		dropdowns = [...dropdowns, dropdowns.length + 1];
+		perkDropdowns = [...perkDropdowns, [1]];  // Add a new array for each new person
+	};
+
+	const addPerk = (index: any) => {
+		perkDropdowns[index] = [...perkDropdowns[index], perkDropdowns[index].length + 1];
+	};
+
+	onMount(async () => {
+		const response = await fetch('/perks.json');
+		perks = await response.json();
+	});
+</script>
+
+<div class="vh-100 d-flex justify-content-center font-monospace">
+	<div class="w-100">
+		<div class="bg-light p-1 text-center">
+			<h1 class="display-6 text-success">Cactus</h1>
+		</div>
+		<form>
+			<div class="w-50 mx-auto p-4">
+				<label for="chance" class="form-label">Chance</label>
+				<input
+					type="number"
+					id="chance"
+					name="chance"
+					class="form-control"
+					placeholder="Enter your chance"
+					max="1"
+				/>
+			</div>
+
+			<div class="w-50 mx-auto p-4">
+				<div class="d-flex justify-content-between align-items-center">
+					<label for="people" class="form-label">People</label>
+					<button type="button" class="btn" on:click={addDropdown}> + </button>
+				</div>
+				{#each dropdowns as dropdown, i (dropdown)}
+					<div class="row m-auto border p-2 rounded mb-3">
+						<button type="button" class="btn btn-primary btn-sm w-25" on:click={() => addPerk(i)}>Add Perk</button>
+						<div class="row m-auto justify-content-center">
+							{#each perkDropdowns[i] as perkDropdown (perkDropdown)}
+								<select class="form-select col-md-4 w-auto m-2">
+									{#each Object.values(perks) as perk}
+										<option value={perk.id}>{perk.name}</option>
+									{/each}
+								</select>
+							{/each}
+						</div>
+					</div>
+				{/each}
+			</div>
+
+			<div class="w-50 mx-auto p-4">
+				<label for="text" class="form-label">Text</label>
+				<input
+					type="text"
+					id="text"
+					name="text"
+					class="form-control"
+					placeholder="Enter your text"
+				/>
+			</div>
+		</form>
+	</div>
+</div>
